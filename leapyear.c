c7d2b31bd98a4a58b1a925f8bff86f4fe7993a96
SelvaganapathiMani <46549629+SelvaganapathiMani@users.noreply.github.com>
2019-02-08 18:33:13 +0530

    Create leapyear.c

---
+++ leapyear.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int c;
+    scanf("%d",&c);
+    
+    if(c%4==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+
+
+    return 0;
+}
